432cf1a213a34e3061c957f2e1e05f70fae5c182
jutt735 <134794853+jutt735@users.noreply.github.com>
2024-05-13 00:00:24 +0500

    Log in
    nothing new

---
+++ app/src/main/java/com/example/fitmate/MainActivity2.kt
@@ -0,0 +1,25 @@
+package com.example.fitmate
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fitmate.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        //startActivity(Intent(this,MainActivity::class.java))
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
